9c3db7cb592f3ad294300357707f657c353ba2e1
Dmitry Teslitsky <dmitryteslitsky@gmail.com>
2019-01-14 20:39:29 +0200

    Remove default audience from JWT guard

--- src/guards/jwt.js
+++ src/guards/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('koa-jwt');
 const { secret } = require('../services/auth/jwt');
 
 // Middleware check auth with passed audience 'client' or 'delivery'
-function guard(audience = 'client') {
+function guard(audience) {
   return jwt({ secret, audience });
 }
 
